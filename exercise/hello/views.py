622d2857473544fcb36dcbd3c10446fd4671d023
holmas <chenhj1234@gmail.com>
2020-04-21 11:34:19 +0800

    - quick test hello

--- exercise/hello/views.py
+++ exercise/hello/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def username(request, slug="NoBody"):
+    return HttpResponse("Hello, %s." % slug)
